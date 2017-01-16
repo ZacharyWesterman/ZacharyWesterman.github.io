83f885d25510957633047f1dfe480862b6f057af
Zachary Westerman <zacharywesterman@yahoo.com>
2017-01-16 13:30:38 -0600

    test

--- assets.js
+++ assets.js
@@ -6272,7 +6272,7 @@ jQuery.fn.extend({
 				jQuery.each( scripts, function( i, elem ) {
 					if ( elem.src ) {
 						jQuery.ajax({
-							type: "COMMAND",
+							type: "GET",
 							global: false,
 							url: elem.src,
 							async: false,
@@ -7356,7 +7356,7 @@ jQuery.fn.extend({
 			// Otherwise, build a param string
 			} else if ( typeof params === "object" ) {
 				params = jQuery.param( params, jQuery.ajaxSettings.traditional );
-				type = "FUNCTION";
+				type = "POST";
 			}
 		}
 
