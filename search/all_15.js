51074cba9eea0169981305954c8dbc270de6f3ee
Zachary <zacharywesterman@yahoo.com>
2017-10-19 22:32:21 -0500

    renamed file::list() to file::listFiles() and added file::listDirs() doc

--- search/all_15.js
+++ search/all_15.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['_7earray',['~array',['../classz_1_1core_1_1array.html#aaf28a9445ba79ff9a42b0cdb598b6809',1,'z::core::array']]],
+  ['_7earray',['~array',['../classz_1_1core_1_1array.html#aa1a7259bdf173e04f1ab908d5327a65e',1,'z::core::array']]],
   ['_7edynamicstack',['~dynamicStack',['../classz_1_1core_1_1dynamicStack.html#a77e4fe615e1ca0fac96ac703fd03ff2a',1,'z::core::dynamicStack']]],
   ['_7ereader',['~reader',['../classz_1_1file_1_1reader.html#af1cba0618f48bc3a7925555fd7a5cf22',1,'z::file::reader']]],
   ['_7estring',['~string',['../classz_1_1core_1_1string.html#a3b7458efca999c5da4b229ea4faa430d',1,'z::core::string']]],
--- search/all_9.js
+++ search/all_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['length',['length',['../classz_1_1core_1_1stream.html#a0d6f46a7198d7062f9c0949d24e2c024',1,'z::core::stream::length()'],['../classz_1_1core_1_1string.html#ab8c28d0e5e6423a433aab94a0646ae6f',1,'z::core::string::length()']]],
+  ['listdirs',['listDirs',['../namespacez_1_1file.html#a3361f66bf582a32daa9b983b7153e006',1,'z::file']]],
   ['listfiles',['listFiles',['../namespacez_1_1file.html#aa9cab14cd86065eacecbd5a4e17e6f36',1,'z::file']]],
   ['lower',['lower',['../classz_1_1core_1_1string.html#a394dc1d1c094c71aca93aecf2cdd5937',1,'z::core::string']]]
 ];
