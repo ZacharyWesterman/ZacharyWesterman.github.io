f17587df2f49674d3f76bdf2f196e2fd55eb0389
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 17:29:24 -0500

    bugfix

--- pc_setup.sh
+++ pc_setup.sh
@@ -3,6 +3,7 @@
 cd ~ || exit 1
 
 if [ ! -e pc_setup__ ]; then
+    rm -f pc_setup.zip pc_setup-*
     wget https://github.com/ZacharyWesterman/pc_setup/archive/refs/heads/main.zip -O pc_setup.zip || exit 1
     unzip pc_setup.zip || exit 1
     rm pc_setup.zip -f
