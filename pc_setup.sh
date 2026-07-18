6d43b6599f0fa0ec9de4beec01f360890f761849
Zachary Westerman <westerman.zachary@gmail.com>
2026-07-18 17:32:16 -0500

    don't allow stdin input

--- pc_setup.sh
+++ pc_setup.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-cd ~ || exit 1
+{
+    cd ~ || exit 1
 
-if [ ! -e pc_setup__ ]; then
-    rm -f pc_setup.zip pc_setup-*
-    wget https://github.com/ZacharyWesterman/pc_setup/archive/refs/heads/main.zip -O pc_setup.zip || exit 1
-    unzip pc_setup.zip || exit 1
-    rm pc_setup.zip -f
-    mv pc_setup-* pc_setup__
-fi
+    if [ ! -e pc_setup__ ]; then
+        rm -f pc_setup.zip pc_setup-*
+        wget https://github.com/ZacharyWesterman/pc_setup/archive/refs/heads/main.zip -O pc_setup.zip || exit 1
+        unzip pc_setup.zip || exit 1
+        rm pc_setup.zip -f
+        mv pc_setup-* pc_setup__
+    fi
 
-cd pc_setup__ || exit 1
+    cd pc_setup__ || exit 1
 
-./install.sh
-./settings.sh
+    ./install.sh
+    ./settings.sh
 
-cd ~ || exit 1
-rm pc_setup__ -rf
+    cd ~ || exit 1
+    rm pc_setup__ -rf
 
-echo
-echo
-echo -e "\033[32mALL SETUP SHOULD BE TOTALLY DONE!\033[0m"
-echo
+    echo
+    echo
+    echo -e "\033[32mALL SETUP SHOULD BE TOTALLY DONE!\033[0m"
+    echo
+} </dev/null
